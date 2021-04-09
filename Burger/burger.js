77b6d0dc6e3312792c1a2bfd5c68bf4db20bc0c2
PatoSala <patosala998@gmail.com>
2021-04-08 22:38:46 -0300

    apple switch

--- Burger/burger.js
+++ Burger/burger.js
@@ -1,3 +1,16 @@
-window.onload(function() {
-    
+window.addEventListener("load",function() {
+
+    let count = 0;
+
+    $(".burger").on("click", function(e) {
+        if (count == 0) {
+            count = 1;
+            $(".first, .mid-level, .last").addClass("click");
+            console.log(count);
+        } else {
+            count = 0;
+            $(".level, .mid-level").removeClass("click");
+            console.log(count);
+        }
+    })
 });
